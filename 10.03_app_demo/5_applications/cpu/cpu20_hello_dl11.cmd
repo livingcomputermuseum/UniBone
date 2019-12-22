c0b6bcafcd7652dd9ed0e3adbdcf5a0f6d74cb91
Joerg Hoppe <j_hoppe@t-online.de>
2019-12-22 17:08:17 +0100

    Cleanup applications

--- 10.03_app_demo/5_applications/cpu/cpu20_hello_dl11.cmd
+++ 10.03_app_demo/5_applications/cpu/cpu20_hello_dl11.cmd
@@ -5,6 +5,13 @@
 dc			# "device with cpu" menu
 
 m i   			# emulate missing memory
+sd dl11
+
+p p ttyS2		# use "UART2 connector, see FAQ
+
+# p b 300		# reduced baudrate
+
+en dl11			# switch on emulated DL11
 
 en cpu20		# switch on emulated 11/20 CPU
 sd cpu20		# select
@@ -16,13 +23,16 @@ p
 init
 
 .print Emulated PDP-11/20 CPU will now output "Hello world"
-.print and enter a serial echo loop on DL11 at 177650.
+.print and enter a serial echo loop on simulated DL11 at 177650.
+.print Regular RS232 port is UART2.
 .print Make sure physical CPU is disabled.
 
 .input
 
 p run 1
 
-.print CPU20 started
+# .print CPU20 started... wait for auto-typed input.
+# dl11 rcv 5000 <This\x20text\x20is\x20typed\x20and\x20echoed\x0d\0x0a>
+
 
 
